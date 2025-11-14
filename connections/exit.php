acc16c7b07aee2f5e990ea1c548679011994ecc2
Vinicius-11340 <vinicius_zakalusny@estudante.sesisenai.org.br>
2025-11-14 12:47:09 -0300

    .

--- connections/exit.php
+++ connections/exit.php
@@ -1,8 +1,11 @@
 <?php
 require_once('db.php');
-
+session_start();
 $_SESSION["user_name"] = "";
 $_SESSION["user_id"] = "";
 $_SESSION["conected"] = false;
+session_unset();
+session_destroy();
 
 header("Location: ../index.php");
+?>
